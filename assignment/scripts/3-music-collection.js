0fa80643463994dabadd1fccb4fe320b6378dbe0
Majid Igale <majidigale18@gmail.com>
2023-04-29 19:29:22 -0500

    finished part 2 and created my catalog

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,9 +12,20 @@ function addToCollection(title,artist, yearPublished ){
     collection.push(album);
     return album;
 }
+//The instructions werent quite clear. It took me too lonng to realize
+// that I just create the object inside the function
 console.log(addToCollection('CLB','Drake', 2023));
 console.log(addToCollection('Sonder Son', 'Brent Faiyaz', 2017));
 console.log(addToCollection('Views','Drake',2016));
 console.log(addToCollection('I Never Liked You', 'Future', 2023));
 console.log(addToCollection('Lil Uzi vs World','Lil Uzi Vert', 2016));
 console.log(addToCollection('Excuse me for being antisocial', 'Roddy Rich', 2020));
+//created the 'showCollections' function. I specified to see how many albums I have
+//first and then my whole collection
+function showCollection(){
+    console.log('This is all of my favorite albums',collection.length)
+    for(i= 0; i< collection.length; i++)
+    console.log(collection[i].title,'by',collection[i].artist, 'published in ', collection[i].yearPublished);
+    
+}
+showCollection(collection);
